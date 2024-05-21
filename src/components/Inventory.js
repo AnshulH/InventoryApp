b4529584eb1a4d2a16f96a96e007930607644892
Anshul Hajare <anshulhajare2024@u.northwestern.edu>
2024-05-21 10:14:50 -0700

    init

---
+++ src/components/Inventory.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Inventory extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    
+}
+
+export default Inventory;
